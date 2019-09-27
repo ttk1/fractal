96a01afea8b2618126682754b6305b5ae9b9d4aa
tama <tama@ttk1.net>
2019-09-28 03:35:38 +0900

    それっぽくなった。

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # fractal
 
+* [シェルピンスキーのギャスケット](https://ja.wikipedia.org/wiki/%E3%82%B7%E3%82%A7%E3%83%AB%E3%83%94%E3%83%B3%E3%82%B9%E3%82%AD%E3%83%BC%E3%81%AE%E3%82%AE%E3%83%A3%E3%82%B9%E3%82%B1%E3%83%83%E3%83%88)
+
 ## コマンド
 
 ```
@@ -13,3 +15,7 @@ npm start
 # jsの出力(dist/配下に出力)
 npm run dist
 ```
+
+## 実行結果
+
+![実行結果](./result.gif)
--- src/glsl/frag.glsl
+++ src/glsl/frag.glsl
@@ -6,24 +6,30 @@ out vec4 color;
 
 uniform vec2 size;
 
-const vec2 p1 = vec2(-1.0, 0.5);
-const vec2 p2 = vec2(0.0, 0.0);
-const vec2 p3 = vec2(0.0, 1.0);
-
-void f(vec2 pos) {}
+const int level = 7;
+const vec2 p1 = vec2(0.0, -1.0);
+const vec2 p2 = vec2(-1.0, 0.5);
+const vec2 p3 = vec2(1.0, 0.5);
 
 void main() {
   vec2 p = gl_FragCoord.xy / size;
-  if (dot(p, p1 - p2) < 0.0 && dot(p, p2 - p3) < 0.0 && dot(p, p3 - p1) < 1.0) {
-    if (dot(p, p2 - p3) < -0.5) {
-      color = vec4(1.0, 1.0, 1.0, 1.0);
-    } else if (dot(p, p1 - p2) < -0.5) {
-      color = vec4(1.0, 1.0, 1.0, 1.0);
-    } else if (dot(p, p3 - p1) < 0.5) {
-      color = vec4(1.0, 1.0, 1.0, 1.0);
-    } else {
-      discard;
+  if (dot(p, p1) < 0.0 && dot(p, p2) < 0.0 && dot(p, p3) < 1.0) {
+    for (int i = 0; i < level; i++) {
+      if (dot(p, p1) < -0.5) {
+        // 上の三角形
+        p = (p - vec2(0.25, 0.5)) * 2.0;
+      } else if (dot(p, p2) < -0.5) {
+        // 右下の三角形
+        p = (p - vec2(0.5, 0.0)) * 2.0;
+      } else if (dot(p, p3) < 0.5) {
+        // 左下の三角形
+        p *= 2.0;
+      } else {
+        discard;
+        break;
+      }
     }
+    color = vec4(1.0, 1.0, 1.0, 1.0);
   } else {
     discard;
   }
