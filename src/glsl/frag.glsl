a2c43817cc8368cfba28794f52cb92a1c4be96ef
tama <tama@ttk1.net>
2019-09-28 02:02:01 +0900

    三角形

--- src/glsl/frag.glsl
+++ src/glsl/frag.glsl
@@ -4,9 +4,16 @@ precision mediump float;
 
 out vec4 color;
 
+const vec2 p1 = vec2(-1.0, 0.5);
+const vec2 p2 = vec2(0.0, 0.0);
+const vec2 p3 = vec2(0.0, 1.0);
+
 void main() {
-  if (distance(gl_FragCoord.xy, vec2(250, 250)) < 50.0) {
+  if (dot(gl_FragCoord.xy / 500.0, p1 - p2) < 0.0 &&
+      dot(gl_FragCoord.xy / 500.0, p2 - p3) < 0.0 &&
+      dot(gl_FragCoord.xy / 500.0, p3 - p1) < 1.0) {
+    color = vec4(1.0, 1.0, 1.0, 1.0);
+  } else {
     discard;
   }
-  color = vec4(1.0, 1.0, 1.0, 1.0);
 }
